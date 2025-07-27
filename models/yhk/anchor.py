6433646943a6980a3ca3655c4a8b42fb9e75abfd
bigbird231 <396016240@qq.com>
2025-07-27 12:25:16 +1000

    model & train & predict init

--- models/yhk/anchor.py
+++ models/yhk/anchor.py
@@ -98,27 +98,7 @@ def multibox_prior(data, scales, ratios):
     # for w*h=960*540 image, return anchor boxes: [30*16,4]
     return output.unsqueeze(0)
 
-
-# show all anchor boxes
-def show_bboxes(axes, bboxes, labels=None, colors=None):
-    def _make_list(obj, default_values=None):
-        if obj is None:
-            obj = default_values
-        elif not isinstance(obj, (list, tuple)):
-            obj = [obj]
-        return obj
-
-    labels = _make_list(labels)
-    colors = _make_list(colors, ['b', 'g', 'r', 'm', 'c'])
-    for i, bbox in enumerate(bboxes):
-        color = colors[i % len(colors)]
-        rect = bbox_to_rect(bbox.detach().numpy(), color)
-        axes.add_patch(rect)
-        # if labels and len(labels) > i:
-        #     text_color = 'k' if color == 'w' else 'w'
-        #     axes.text(rect.xy[0], rect.xy[1], labels[i], va='center', ha='center', fontsize=9, color=text_color, bbox=dict(facecolor=color, lw=0))
-
-
+# width * height
 def calc_box_area(boxes):
     return (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
 
---
+++ models/yhk/train.py
@@ -0,0 +1,59 @@
+import torch
+import torchvision
+from torch import nn
+from torch.nn import functional as F
+from anchor import multibox_prior, multibox_target
+from model import trainer
+
+
+cls_loss = nn.CrossEntropyLoss(reduction='none')
+bbox_loss = nn.L1Loss(reduction='none')
+
+def calc_loss(cls_preds, cls_labels, bbox_preds, bbox_labels, bbox_masks):
+    batch_size, num_classes = cls_preds.shape[0], cls_preds.shape[2]
+    cls = cls_loss(cls_preds.reshape(-1, num_classes),
+                   cls_labels.reshape(-1)).reshape(batch_size, -1).mean(dim=1)
+    bbox = bbox_loss(bbox_preds * bbox_masks,
+                     bbox_labels * bbox_masks).mean(dim=1)
+    return cls + bbox
+
+
+
+def cls_eval(cls_preds, cls_labels):
+    return float((cls_preds.argmax(dim=-1).type(
+        cls_labels.dtype) == cls_labels).sum())
+
+def bbox_eval(bbox_preds, bbox_labels, bbox_masks):
+    return float((torch.abs((bbox_labels - bbox_preds) * bbox_masks)).sum())
+
+
+num_epochs, timer = 20, d2l.Timer()
+animator = d2l.Animator(xlabel='epoch', xlim=[1, num_epochs],
+                        legend=['class error', 'bbox mae'])
+net = net.to(device)
+for epoch in range(num_epochs):
+    # 训练精确度的和，训练精确度的和中的示例数
+    # 绝对误差的和，绝对误差的和中的示例数
+    metric = d2l.Accumulator(4)
+    net.train()
+    for features, target in train_iter:
+        timer.start()
+        trainer.zero_grad()
+        X, Y = features.to(device), target.to(device)
+        # 生成多尺度的锚框，为每个锚框预测类别和偏移量
+        anchors, cls_preds, bbox_preds = net(X)
+        # 为每个锚框标注类别和偏移量
+        bbox_labels, bbox_masks, cls_labels = d2l.multibox_target(anchors, Y)
+        # 根据类别和偏移量的预测和标注值计算损失函数
+        l = calc_loss(cls_preds, cls_labels, bbox_preds, bbox_labels,
+                      bbox_masks)
+        l.mean().backward()
+        trainer.step()
+        metric.add(cls_eval(cls_preds, cls_labels), cls_labels.numel(),
+                   bbox_eval(bbox_preds, bbox_labels, bbox_masks),
+                   bbox_labels.numel())
+    cls_err, bbox_mae = 1 - metric[0] / metric[1], metric[2] / metric[3]
+    animator.add(epoch + 1, (cls_err, bbox_mae))
+print(f'class err {cls_err:.2e}, bbox mae {bbox_mae:.2e}')
+print(f'{len(train_iter.dataset) / timer.stop():.1f} examples/sec on '
+      f'{str(device)}')
